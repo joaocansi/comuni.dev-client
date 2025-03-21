7c3334e759361b6b9a23646cfa4eb3d776742502
joaocansi <whitshs2003@gmail.com>
2025-03-21 12:31:58 -0300

    feat: test

--- src/app/comunidades/layout.tsx
+++ src/app/comunidades/layout.tsx
@@ -11,6 +11,7 @@ export default async function RootLayout({
   const session = await authClient.getSession({
     fetchOptions: {
       headers: await headers(),
+      credentials: "include",
     },
   });
 
