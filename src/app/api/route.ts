11dc3d6b9394807cfd31fe7c34d199388cd02df4
joaocansi <whitshs2003@gmail.com>
2025-03-21 12:09:29 -0300

    feat: test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,12 @@
+import { headers } from "next/headers";
+
 import { api } from "@/src/shared/clients/api-client";
 
 export async function GET() {
+  const c = await headers();
+
   const response = await api.get(`/communities/golangers`);
   const data = response.data;
 
-  return Response.json({ data });
+  return Response.json({ data, c });
 }
