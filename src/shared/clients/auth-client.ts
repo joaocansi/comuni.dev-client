135fedc67ad3c6abc531c48fec0341317e1ae2e6
joaocansi <whitshs2003@gmail.com>
2025-03-21 16:25:32 -0300

    feaT: sts

--- src/shared/clients/auth-client.ts
+++ src/shared/clients/auth-client.ts
@@ -3,6 +3,6 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   fetchOptions: {
-    credentials: "include",
+    cache: "no-store",
   },
 });
